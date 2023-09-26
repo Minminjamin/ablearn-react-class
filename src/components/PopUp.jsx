599aea6a006312033e038ce2573c8fe9e65f1cfc
Minminjamin <0429el@gmail.com>
2023-09-26 10:43:58 +0900

    etc : useEffect 학습 에제

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style.scss";
 
 const PopUp = () => {
-  return <aside>PopUp</aside>;
+  const [Num, setNum] = useState(0);
+  useEffect(() => {
+    console.log("popup생성");
+
+    return () => {
+      console.log("popup소멸");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("popup의 Num 상태변화");
+  }, [Num]);
+
+  return (
+    <aside>
+      <h1>{Num}</h1>
+      <button onClick={() => setNum(Num - 1)}>minus</button>
+      <button onClick={() => setNum(Num + 1)}>plus</button>
+    </aside>
+  );
 };
 
 export default PopUp;
